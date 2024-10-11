7f0e9cd15df86aee80d055ae00ed255337b88797
Daddy2054 <stivali-tinte-0w@icloud.com>
2024-10-11 18:29:13 +0200

    Initial Commit

---
+++ ScoreKeeper/ScoreKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreKeeperApp.swift
+//  ScoreKeeper
+//
+//  Created by Jean on 11/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ScoreKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
